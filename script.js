07b639d71668505d6b2bb99fa909613bc1a8e643
ChenJuLei <1282783433@qq.com>
2022-12-31 20:28:36 +0800

    新年快乐网页

--- script.js
+++ script.js
@@ -1,200 +1,579 @@
-MorphSVGPlugin.convertToPath('polygon');
-var xmlns = "http://www.w3.org/2000/svg",
-  xlinkns = "http://www.w3.org/1999/xlink",
-select = function(s) {
-    return document.querySelector(s);
-  },
-  selectAll = function(s) {
-    return document.querySelectorAll(s);
-  },
-  pContainer = select('.pContainer'),
-  mainSVG = select('.mainSVG'),
-  star = select('#star'),
-  sparkle = select('.sparkle'),
-  tree = select('#tree'),
-  showParticle = true,
-  particleColorArray = ['#E8F6F8', '#ACE8F8', '#F6FBFE','#A2CBDC','#B74551', '#5DBA72', '#910B28', '#910B28', '#446D39'],
-  particleTypeArray = ['#star','#circ','#cross','#heart'],
- // particleTypeArray = ['#star'],
-  particlePool = [],
-  particleCount = 0,
-  numParticles = 201
+function init() {
+  var svg = document.querySelector(".scene");
+  var zoom = false;
+  var animationOn = false;
 
+  var viewBoxes = {
+    "overHouses": {
+      x: 43,
+      y: 290,
+      width: 130,
+      height: 67
+    },
+    "overSnowmen": {
+      x: 250,
+      y: 325,
+      width: 225,
+      height: 115
+    },
+    "overPenguins": {
+      x: 634,
+      y: 310,
+      width: 95,
+      height: 140
+    },
+    "overHanging": {
+      x: 774,
+      y: 416,
+      width: 49,
+      height: 38
+    },
+    "overSkilift": {
+      x: 897,
+      y: 284,
+      width: 217,
+      height: 130,
+    }
+  };
 
-gsap.set('svg', {
-  visibility: 'visible'
-})
+  var skiliftDom = svg.querySelector("#skilift"),
+    houses = {
+      dom: svg.querySelector("#houses")
+    },
+    penguinsDom = svg.querySelectorAll("#penguins > g"),
+    babyPenguins = [penguinsDom[2], penguinsDom[3], penguinsDom[4]],
+    snowManHat = svg.querySelector("#hatman"),
+    handHat = snowManHat.querySelector("#handhat_1_"),
+    eyesHat = snowManHat.querySelector("#eyesHatMan"),
+    elfMan = svg.querySelector("#elfman"),
+    elfButtons = elfMan.querySelectorAll("#elfButtons circle"),
+    elfBow = elfMan.querySelectorAll("#bowElf"),
+    scarfMan = svg.querySelector("#scarfman"),
+    hangingDom = svg.querySelectorAll("#hanging > g"),
+    letters = svg.querySelectorAll("#letters path");
 
-gsap.set(sparkle, {
-	transformOrigin:'50% 50%',
-	y:-100
-})
+  function startScene() {
 
-let getSVGPoints = (path) => {
-	
-	let arr = []
-	var rawPath = MotionPathPlugin.getRawPath(path)[0];
-	rawPath.forEach((el, value) => {
-		let obj = {}
-		obj.x = rawPath[value * 2]
-		obj.y = rawPath[(value * 2) + 1]
-		if(value % 2) {
-			arr.push(obj)
-		}
-		//console.log(value)
-	})
-	
-	return arr;
-}
-let treePath = getSVGPoints('.treePath'),
-    treeBottomPath = getSVGPoints('.treeBottomPath'),
-    mainTl = gsap.timeline({delay:0, repeat:0}),
-    starTl;
+    TweenMax.set([svg.querySelector("#leftSkiLift"), svg.querySelector("#rightSkiLift")], {
+      y: -600
+    });
+    TweenMax.set(svg.querySelector("#trailSkiLift"), {
+      scaleX: 0,
+      transformOrigin: "left bottom"
+    });
+    TweenMax.set(svg.querySelector("#lift"), {
+      opacity: 0
+    });
+    TweenMax.set(houses.dom, {
+      scaleY: 0,
+      transformOrigin: "center bottom"
+    });
+    TweenMax.set(svg.querySelector("#snowPenguins"), {
+      y: -600,
+      opacity: 0
+    });
+    TweenMax.set(svg.querySelector("#penguins"), {
+      scale: 0,
+      transformOrigin: "10% 50%"
+    });
+    TweenMax.set([svg.querySelector("#snowManSnow1"), svg.querySelector("#snowManSnow2")], {
+      y: -600,
+      opacity: 0
+    });
+    TweenMax.set(snowManHat, {
+      rotationZ: -60,
+      scale: 0,
+      transformOrigin: "center bottom"
+    });
+    TweenMax.set(elfMan, {
+      rotationZ: 60,
+      scale: 0,
+      transformOrigin: "center bottom"
+    });
+    TweenMax.set(scarfMan, {
+      rotationZ: 90,
+      scale: 0,
+      transformOrigin: "center bottom"
+    });
+    TweenMax.set(hangingDom, {
+      opacity: 0
+    });
 
-function flicker(p){
+    //Letters intro
+    for (var i = 0; i < letters.length; i++) {
+      TweenMax.set(letters[i], {
+        transformOrigin: "center top",
+        rotationZ: Math.random() * 180,
+        scale: 0,
+      });
+    }
 
-  gsap.killTweensOf(p, {opacity:true});
-  gsap.fromTo(p, {
-    opacity:1
-  }, {
-		duration: 0.07,
-    opacity:Math.random(),
-    repeat:-1
-  })
-}
-
-function createParticles() {
-  
-  var i = numParticles, p, particleTl, step = numParticles/treePath.length, pos;
-  while (--i > -1) {
-    
-    p = select(particleTypeArray[i%particleTypeArray.length]).cloneNode(true);
-    mainSVG.appendChild(p);
-    p.setAttribute('fill', particleColorArray[i % particleColorArray.length]);
-    p.setAttribute('class', "particle");   
-    particlePool.push(p);
-    //hide them initially
-    gsap.set(p, {
-                 x:-100, 
-                 y:-100,
-   transformOrigin:'50% 50%'
-                 })
-    
-    
+    copyAnim();
 
   }
 
-}
-
-var getScale = gsap.utils.random(0.5, 3, 0.001, true);
-
-function playParticle(p){
-  if(!showParticle){return};
-  var p = particlePool[particleCount]
- gsap.set(p, {
-	 x: gsap.getProperty('.pContainer', 'x'),
-	 y: gsap.getProperty('.pContainer', 'y'),
-	 scale:getScale()
-    }
-    );
-var tl = gsap.timeline();
-  tl.to(p, {
-		duration: gsap.utils.random(0.61,6),
-      physics2D: {
-        velocity: gsap.utils.random(-23, 23),
-        angle:gsap.utils.random(-180, 180),
-        gravity:gsap.utils.random(-6, 50)
-      },
-      scale:0,
-      rotation:gsap.utils.random(-123,360),
-      ease: 'power1',
-      onStart:flicker,
-      onStartParams:[p],
-      onRepeat: (p) => {
-        gsap.set(p, {         
-            scale:getScale()
-        })
-      },
-      onRepeatParams: [p]
-
+  function openBox() {
+    document.querySelector(".gift").removeEventListener("click", openBox);
+    TweenMax.set(".hat", {
+      transformOrigin: "left bottom"
     });
-  
+    TweenMax.to(".hat", 1, {
+      rotationZ: -80,
+      x: -500,
+      opacity:0,
+      ease: Power2.easeIn
+    });
+    TweenMax.to(".box", 1, {
+      y: 800,
+      ease: Power2.easeIn
+    });
+    TweenMax.to(".gift", 1, {
+      opacity: 0,
+      delay: 1,
+      onStart: function() {
+        startScene();
+      },
+      onComplete: function() {
+        document.querySelector(".gift").classList.add("hidden");
+      }
+    });
+  }
 
-  particleCount++;
-  particleCount = (particleCount >=numParticles) ? 0 : particleCount
-  
+  function startAnimations() {
+    animationOn = true;
+
+    //Ski Lift
+    var skilift = new TimelineMax({
+      repeat: -1,
+      repeatDelay: 0.5
+    });
+    skilift.to(skiliftDom.querySelector("#lift"), 7, {
+      x: 145,
+      y: -64,
+      ease: Power1.easeInOut
+    });
+    skilift.to(skiliftDom.querySelector("#lift"), 7, {
+      x: 0,
+      y: 0,
+      ease: Power1.easeInOut
+    }, "+=.5");
+
+    //Ski Lift
+    houses.smoke1 = houses.dom.querySelectorAll("#smokes > g:nth-child(5) path");
+    houses.smoke2 = houses.dom.querySelectorAll("#smokes > g:nth-child(4) path");
+    houses.smoke3 = houses.dom.querySelectorAll("#smokes > g:nth-child(3) path");
+    houses.smoke4 = houses.dom.querySelectorAll("#smokes > g:nth-child(2) path");
+    houses.smoke5 = houses.dom.querySelectorAll("#smokes > g:nth-child(1) path");
+    TweenMax.staggerTo(houses.smoke1, 4, {
+      y: -11,
+      opacity: 0,
+      repeat: -1,
+      ease: Linear.easeNone
+    }, 0.5);
+    TweenMax.staggerTo(houses.smoke2, 4, {
+      y: -13,
+      opacity: 0,
+      repeat: -1,
+      ease: Linear.easeNone
+    }, 0.5);
+    TweenMax.staggerTo(houses.smoke3, 4, {
+      y: -9,
+      opacity: 0,
+      repeat: -1,
+      ease: Linear.easeNone
+    }, 0.8);
+    TweenMax.staggerTo(houses.smoke4, 4, {
+      y: -14,
+      opacity: 0,
+      repeat: -1,
+      ease: Linear.easeNone
+    }, 1);
+    TweenMax.staggerTo(houses.smoke5, 7, {
+      y: -17,
+      opacity: 0,
+      repeat: -1,
+      ease: Linear.easeNone
+    }, 1.3);
+
+    //Hanging
+    var hanging = new TimelineMax({
+      repeat: -1
+    });
+    TweenMax.set(hangingDom, {
+      transformOrigin: "center top"
+    });
+    TweenMax.set(hangingDom[0], {
+      rotationZ: 10
+    });
+    TweenMax.set(hangingDom[1], {
+      rotationZ: 5
+    });
+    TweenMax.set(hangingDom[2], {
+      rotationZ: -13
+    });
+    TweenMax.set(hangingDom[3], {
+      rotationZ: -8
+    });
+    TweenMax.set(hangingDom[4], {
+      rotationZ: 15
+    });
+    TweenMax.to(hangingDom[0], 3, {
+      rotationZ: -10,
+      ease: Power1.easeInOut,
+      repeat: -1,
+      yoyo: true
+    });
+    TweenMax.to(hangingDom[1], 2, {
+      rotationZ: -5,
+      ease: Power1.easeInOut,
+      repeat: -1,
+      yoyo: true
+    });
+    TweenMax.to(hangingDom[2], 5, {
+      rotationZ: 13,
+      ease: Power1.easeInOut,
+      repeat: -1,
+      yoyo: true
+    });
+    TweenMax.to(hangingDom[3], 4, {
+      rotationZ: 8,
+      ease: Power1.easeInOut,
+      repeat: -1,
+      yoyo: true
+    });
+    TweenMax.to(hangingDom[4], 3, {
+      rotationZ: -15,
+      ease: Power1.easeInOut,
+      repeat: -1,
+      yoyo: true
+    });
+
+    //Pinguins
+    var penguins1 = new TimelineMax({
+      repeat: -1
+    });
+    penguins1.to(penguinsDom[0], 3, {
+      x: 35,
+      y: 90,
+      ease: Power2.easeIn
+    });
+    penguins1.set(penguinsDom[0], {
+      x: 0,
+      y: 0,
+      opacity: 0,
+      scale: 0
+    });
+    penguins1.to(penguinsDom[0], 0.7, {
+      opacity: 1,
+      scale: 1
+    }, "+=1");
+
+    var penguins2 = new TimelineMax({
+      repeat: -1,
+      delay: 1
+    });
+    penguins2.to(penguinsDom[1], 3, {
+      x: 34,
+      y: 90,
+      ease: Power2.easeIn
+    });
+    penguins2.set(penguinsDom[1], {
+      x: 0,
+      y: 0,
+      opacity: 0,
+      scale: 0
+    });
+    penguins2.to(penguinsDom[1], 0.7, {
+      opacity: 1,
+      scale: 1
+    }, "+=1");
+
+    var penguins3 = new TimelineMax({
+      repeat: -1
+    });
+    penguins3.staggerTo(babyPenguins, 4, {
+      x: 39,
+      y: 100,
+      ease: Power2.easeIn
+    }, 1.1);
+    penguins3.set(babyPenguins, {
+      opacity: 0,
+      x: 0,
+      y: 0,
+      scale: 0
+    });
+    penguins3.staggerTo(babyPenguins, 0.7, {
+      opacity: 1,
+      scale: 1
+    }, .2);
+    penguins3.progress(.5);
+
+    //Snowman hat
+    TweenMax.set(handHat, {
+      transformOrigin: "70% bottom"
+    });
+    TweenMax.to(handHat, 3, {
+      rotationZ: 20,
+      yoyo: true,
+      repeat: -1,
+      ease: Power1.easeInOut
+    });
+    TweenMax.set(eyesHat, {
+      transformOrigin: "center center"
+    });
+    var eyesHatTl = new TimelineMax({
+      repeat: -1,
+      repeatDelay: 1.5
+    });
+    eyesHatTl.to(eyesHat, 0.1, {
+      scaleY: 0
+    });
+    eyesHatTl.to(eyesHat, 0.1, {
+      scaleY: 1
+    });
+
+    //Snowman elf
+    var elfButtonsTl = new TimelineMax({
+      repeat: -1
+    });
+    elfButtonsTl.set(elfButtons[0], {
+      fill: "#EE4250"
+    });
+    elfButtonsTl.set(elfButtons[0], {
+      fill: "#22BC34"
+    }, "+=0.3");
+    elfButtonsTl.set(elfButtons[1], {
+      fill: "#EE4250"
+    });
+    elfButtonsTl.set(elfButtons[1], {
+      fill: "#22BC34"
+    }, "+=0.3");
+    elfButtonsTl.set(elfButtons[2], {
+      fill: "#EE4250"
+    });
+    elfButtonsTl.set(elfButtons[2], {
+      fill: "#22BC34"
+    }, "+=0.3");
+
+    TweenMax.set(elfBow, {
+      transformOrigin: "center center"
+    })
+    var elfBowTl = new TimelineMax({
+      repeat: -1,
+      repeatDelay: .4
+    });
+    elfBowTl.to(elfBow, 0.4, {
+      scaleX: 1.6
+    });
+    elfBowTl.to(elfBow, 0.8, {
+      scaleX: 1,
+      ease: Elastic.easeOut
+    });
+
+    TweenMax.set(elfMan.querySelector("#elfLeftArm"), {
+      transformOrigin: "right top",
+      rotationZ: 55
+    });
+    TweenMax.to(elfMan.querySelector("#elfLeftArm"), 1, {
+      rotationZ: 100,
+      yoyo: true,
+      repeat: -1,
+      ease: Power1.easeInOut
+    });
+    TweenMax.set(elfMan.querySelector("#elfRightArm"), {
+      transformOrigin: "left top",
+      rotationZ: -60
+    });
+    TweenMax.to(elfMan.querySelector("#elfRightArm"), 1, {
+      rotationZ: -110,
+      yoyo: true,
+      repeat: -1,
+      ease: Power1.easeInOut,
+      delay: -0.21
+    });
+
+    //Snowman scard
+    TweenMax.to(scarfMan.querySelector("#pieceScarfMan"), 3, {
+      rotationZ: -18,
+      yoyo: true,
+      repeat: -1,
+      ease: Linear.easeNone
+    });
+
+    var scarfManTl = new TimelineMax({
+      repeat: -1,
+      repeatDelay: 1
+    });
+    TweenMax.set(scarfMan, {
+      transformOrigin: "center bottom"
+    });
+    scarfManTl.to(scarfMan, 0.2, {
+      scaleY: 0.92
+    });
+    scarfManTl.to(scarfMan, 0.6, {
+      scaleY: 1.07,
+      y: -8
+    });
+    scarfManTl.to(scarfMan, 0.4, {
+      scaleY: 1,
+      y: 0,
+      ease: Power1.easeIn
+    });
+
+  }
+
+  //SNOW
+  var snow = svg.querySelector("#snow").querySelectorAll("ellipse, path");
+  var snows = [
+    [],
+    []
+  ];
+  for (var i = 0; i < snow.length; i++) {
+    rand = Math.floor(Math.random() * 2);
+    var offset = snow[i].getBoundingClientRect();
+    TweenMax.set(snow[i], {
+      y: -(offset.top + offset.width + 100)
+    });
+    TweenMax.to(snow[i], Math.random() * 50 + 50, {
+      y: 920,
+      repeat: -1,
+      delay: -Math.random() * 100
+    });
+  }
+
+  function copyAnim() {
+    var appearance = new TimelineMax({
+      onComplete: startAnimations
+    }).timeScale(1);
+    appearance.staggerTo(letters, 3, {
+        scale: 1,
+        rotationZ: 0,
+        ease: Elastic.easeOut
+      }, 0.1)
+      .to(houses.dom, 1, {
+        scaleY: 1,
+        ease: Elastic.easeOut
+      }, "-=2")
+      .to(svg.querySelector("#snowManSnow1"), 1, {
+        opacity: 1,
+        y: 0,
+        ease: Power2.easeOut
+      }, "-=1.7")
+      .to(svg.querySelector("#snowManSnow2"), 1, {
+        opacity: 1,
+        y: 0,
+        ease: Power2.easeOut
+      }, "-=1.2")
+      .to(scarfMan, 1, {
+        rotationZ: 0,
+        scale: 1,
+        ease: Elastic.easeOut
+      }, "-=0.8")
+      .to(snowManHat, 1, {
+        rotationZ: 0,
+        scale: 1,
+        ease: Elastic.easeOut
+      }, "-=0.8")
+      .to(elfMan, 1, {
+        rotationZ: 0,
+        scale: 1,
+        ease: Elastic.easeOut
+      }, "-=0.8")
+      .to(svg.querySelector("#snowPenguins"), 1, {
+        opacity: 1,
+        y: 0,
+        ease: Power2.easeOut
+      }, "-=.8")
+      .to(svg.querySelector("#penguins"), 1, {
+        scale: 1,
+        ease: Power2.easeOut
+      })
+      .to(hangingDom, 0.3, {
+        opacity: 1
+      })
+      .to(svg.querySelector("#leftSkiLift"), 0.7, {
+        y: 0,
+        ease: Power1.easeOut
+      })
+      .to(svg.querySelector("#rightSkiLift"), 0.7, {
+        y: 0,
+        ease: Power1.easeOut
+      })
+      .to(svg.querySelector("#trailSkiLift"), 0.7, {
+        scaleX: 1
+      })
+      .to(svg.querySelector("#lift"), 0.3, {
+        opacity: 1
+      }, "-=0.7")
+  }
+
+  //HOVER EFFECTS
+  function enterOverlay(e) {
+    if (!zoom && animationOn) {
+      svg.style.cursor = "url(https://s3-us-west-2.amazonaws.com/s.cdpn.io/127738/glass-01.png) 20 20, auto";
+      TweenMax.to(e.target, .3, {
+        opacity: 1,
+        ease: Power2.easeOut
+      });
+    }
+  }
+
+  function leaveOverlay(e, el) {
+    if (!el) {
+      el = e.target;
+    }
+    if (!zoom) {
+      svg.style.cursor = "auto";
+    }
+    TweenMax.to(el, .5, {
+      opacity: 0,
+      ease: Power2.easeOut
+    });
+  }
+
+  function zoomViewBox(e) {
+    if (zoom || !animationOn) {
+      return;
+    }
+    e.stopPropagation();
+    var id = this.getAttribute("id");
+    
+    TweenMax.to(svg, 2, {attr:{viewBox: viewBoxes[id].x + " " + viewBoxes[id].y + " " + viewBoxes[id].width + " " + viewBoxes[id].height},
+      ease: Power3.easeOut
+    });
+    
+    zoom = true;
+    leaveOverlay(false, this);
+    svg.style.cursor = "url(https://s3-us-west-2.amazonaws.com/s.cdpn.io/127738/cross.png) 20 20, auto";
+  }
+
+  function unZoom() {
+    zoom = false;
+    svg.style.cursor = "auto";
+     TweenMax.to(svg, 2, {attr:{viewBox:"0 0 1600 900"}, ease: Power3.easeOut});
+  }
+
+  var overlays = svg.querySelectorAll("#overlays > g");
+  for (var i = 0; i < overlays.length; i++) {
+    overlays[i].addEventListener("mouseenter", enterOverlay);
+    overlays[i].addEventListener("mouseleave", leaveOverlay);
+    overlays[i].addEventListener("click", zoomViewBox);
+  }
+  svg.addEventListener("click", unZoom);
+
+  document.querySelector(".gift").addEventListener("click", openBox);
+  document.body.style.background = "#bfe2dc";
+  document.querySelector(".scene").style.display = "block";
+
 }
 
-function drawStar(){
-  
-  starTl = gsap.timeline({onUpdate:playParticle})
-  starTl.to('.pContainer, .sparkle', {
-		duration: 6,
-		motionPath :{
-			path: '.treePath',
-      autoRotate: false
-		},
-    ease: 'linear'
-  })  
-  .to('.pContainer, .sparkle', {
-		duration: 1,
-    onStart:function(){showParticle = false},
-    x:treeBottomPath[0].x,
-    y:treeBottomPath[0].y
-  })
-  .to('.pContainer, .sparkle',  {
-		duration: 2,
-    onStart:function(){showParticle = true},
-		motionPath :{
-			path: '.treeBottomPath',
-      autoRotate: false
-		},
-    ease: 'linear'    
-  },'-=0')
-.from('.treeBottomMask', {
-		duration: 2,
-  drawSVG:'0% 0%',
-  stroke:'#FFF',
-  ease:'linear'
-},'-=2')  
-   
-}
+//preload images
+var glass = new Image()
+glass.src = "https://s3-us-west-2.amazonaws.com/s.cdpn.io/127738/glass-01.png";
+var cross = new Image()
+cross.src = "https://s3-us-west-2.amazonaws.com/s.cdpn.io/127738/cross.png";
 
-
-createParticles();
-drawStar();
-
-mainTl.from(['.treePathMask','.treePotMask'],{
-	duration: 6,
-  drawSVG:'0% 0%',
-  stroke:'#FFF',
-	stagger: {
-		each: 6
-	},
-  duration: gsap.utils.wrap([6, 1,2]),
-  ease:'linear'
-})
-.from('.treeStar', {
-	duration: 3,
-  scaleY:0,
-  scaleX:0.15,
-  transformOrigin:'50% 50%',
-  ease: 'elastic(1,0.5)'
-},'-=4')
-.to(['.treeStarOutline', '.messageText'], 1, {
-  alpha:1,
-  ease:RoughEase.ease.config({ template:  Power0.easeNone, strength: 2, points: 16, taper: "none", randomize: true, clamp: false})
-},'-=3')
- .to('.sparkle', {
-	duration: 3,
-    opacity:0,
-    ease:"rough({strength: 2, points: 100, template: linear, taper: both, randomize: true, clamp: false})"
-  },'-=0')
-  .to('.treeStarOutline', {
-	duration: 1,
-    opacity:1,
-    ease:"rough({strength: 2, points: 16, template: linear, taper: none, randomize: true, clamp: false})"
-  },'+=1')
-
-mainTl.add(starTl, 0)
-gsap.globalTimeline.timeScale(1.5);
+          
+window.onload = function() {
+  init();
+};
